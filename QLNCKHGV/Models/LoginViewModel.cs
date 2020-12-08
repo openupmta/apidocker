c80b1ea7a535af48fae220dd30dd5e22b12fc005
openupmta <openupmta99@gmail.com>
2020-12-08 20:45:14 +0700

    update

---
+++ QLNCKHGV/Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace QLNCKHGV.Models
+{
+    public class LoginViewModel
+    {
+        [StringLength(50)]
+        public string UserName { get; set; }
+
+        [StringLength(50)]
+        public string Password { get; set; }
+    }
+
+}
